4151220e2e4ab8e460b9a26242564f6b70885b2d
Martín Alejandro Mednik <mmednik@gmail.com>
2023-02-10 16:43:00 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const users = require("../users.json");
+module.exports = app;
+
+// GET /users endpoint
+app.get("/users", (req, res) => {
+  res.send(users.filter((user) => user.id == req.query.id));
+});
